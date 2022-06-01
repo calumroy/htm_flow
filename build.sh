cc72d15164110e69b88c247d7e4c740729bcc54f
calumroy <calum@calumroy.com>
2022-06-01 22:07:15 +0800

    First build of taskflow example

--- build.sh
+++ build.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 # Go to library source tree root and run the usual:
 # make this an executable with chmod +x ./build.sh
-mkdir build && cd $_
+
+# A bash script to create a build dir if it doesn't exist and then move into it.
+# If the build dir exists, it will be deleted and recreated.
+
+# Make the build dir if it doesn't exist.
+mkdir -p build
+# Move into the build dir.
+cd build
 cmake -DCMAKE_BUILD_TYPE=Debug ..
 cmake --build .
