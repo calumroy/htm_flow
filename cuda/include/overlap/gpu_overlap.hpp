e63d10dea07ae3ebf178cca3168c84a2d8a43f0f
calumroy <calum@calumroy.com>
2024-07-06 19:32:16 +0800

    COmmented out the GPU opt overlap sparse functions. It does not work for certain input sizes. NEeds a rethink on how to more efficiently claculate the sparse version on the GPU. Maybe we should just use a CPU sparse overlap calcuation instead.

--- cuda/include/overlap/gpu_overlap.hpp
+++ cuda/include/overlap/gpu_overlap.hpp
@@ -89,16 +89,16 @@ namespace gpu_overlap
     /// @param[in] optimised        Optional param, whether to use the optimised version of the kernel function or not.
     void initialize_gpu_memory(int in_rows, int in_cols, int N, int M, int O, int P, bool optimised = false);
     
-    ///-----------------------------------------------------------------------------
-    ///
-    /// initialize_gpu_memory_sparse    Initialize the GPU memory for the input and output data for the calculate_overlap_gpu_stream_opt_sparse function.
-    ///
-    /// @param[in] num_active           The number of active elements in the input data.
-    /// @param[in] N                    The number of columns in the cortical columns.
-    /// @param[in] M                    The number of rows in the cortical columns.
-    /// @param[in] O                    The number of rows in the neighbourhood patch.
-    /// @param[in] P                    The number of columns in the neighbourhood patch.
-    void initialize_gpu_memory_sparse(int num_active, int N, int M, int O, int P);
+    // ///-----------------------------------------------------------------------------
+    // ///
+    // /// initialize_gpu_memory_sparse    Initialize the GPU memory for the input and output data for the calculate_overlap_gpu_stream_opt_sparse function.
+    // ///
+    // /// @param[in] num_active           The number of active elements in the input data.
+    // /// @param[in] N                    The number of columns in the cortical columns.
+    // /// @param[in] M                    The number of rows in the cortical columns.
+    // /// @param[in] O                    The number of rows in the neighbourhood patch.
+    // /// @param[in] P                    The number of columns in the neighbourhood patch.
+    // void initialize_gpu_memory_sparse(int num_active, int N, int M, int O, int P);
 
 
     ///-----------------------------------------------------------------------------
@@ -108,10 +108,10 @@ namespace gpu_overlap
     /// @param[in] optimised          Optional param, whether to use the optimised version of the kernel function or not.
     void cleanup_gpu_memory(bool optimised = false);
 
-    ///-----------------------------------------------------------------------------
-    ///
-    /// cleanup_gpu_memory_sparse    Free the GPU memory for the input and output data for the calculate_overlap_gpu_stream_opt_sparse function.
-    void cleanup_gpu_memory_sparse();
+    // ///-----------------------------------------------------------------------------
+    // ///
+    // /// cleanup_gpu_memory_sparse    Free the GPU memory for the input and output data for the calculate_overlap_gpu_stream_opt_sparse function.
+    // void cleanup_gpu_memory_sparse();
 
     ///-----------------------------------------------------------------------------
     ///
@@ -198,53 +198,53 @@ namespace gpu_overlap
                                         std::vector<float> &out_pot_overlap
                                     );
     
-    // Struct to represent a 2D integer coordinate. 
-    // Used as an input type in the calculate_overlap_gpu_stream_opt_sparse function.
-    // This struct is equivalent to cuda::int2 struct and is converted to cuda::int2 in the function calculate_overlap_gpu_stream_opt_sparse.
-    struct Int2 {
-        int x;
-        int y;
-    };
-    //-----------------------------------------------------------------------------
-    ///
-    /// calculate_overlap_gpu_stream_opt_sparse   Calculate the overlap scores for a given sparse input.
-    ///                                          This function calculates how well each cortical column connects to the active inputs.
-    ///                                          It's optimized for streaming sparse data, where active inputs are a small subset of the total.
-    ///                                          The function is designed for efficient processing with varying active input patterns but
-    ///                                          fixed structural parameters (like cortical column dimensions and neighborhood size).
-    ///                                          It uses a list of active elements instead of a dense input grid to minimize memory usage
-    ///                                          and computation on inactive elements. 
-    ///                                          It also uses a colConBits input parameter. This is a vector of uint32_t where each bit in the array represents the connection bit for a synapse in the neighbourhood.
-    ///                                          This input parameter is designed to reduce the GPU memory footprint of the connected state of each synpase.
-    ///
-    /// @param[in] columns_width           The width of the cortical columns (number of columns in the output matrix).
-    /// @param[in] columns_height          The height of the cortical columns (number of rows in the output matrix).
-    /// @param[in] colConBits              A vector of uint32_t where each bit represents the connection state of a synapse in the neighbourhood.
-    /// @param[in] active_grid             A vector of int2 structures representing the active elements' coordinates in the input grid.
-    /// @param[in] num_active              The number of active elements in the active_grid.
-    /// @param[in] inputGrid_shape         The shape of the full input grid (height, width) as a pair of ints, for context in processing the sparse active elements.
-    /// @param[in] neib_shape              The shape of the neighbourhood around each cortical column.
-    /// @param[in] neib_step               The step size of the sliding window over the input grid.
-    /// @param[in] wrap_mode               Whether to wrap the neighbourhood around the edges (toroidal topology) or not.
-    /// @param[in] center_neigh            Whether to center the neighbourhood around each active element or start from the corner.
-    /// @param[out] out_overlap            The overlap scores for each column, output as a 1D vector simulating a 2D matrix.
-    /// @param[out] out_pot_overlap        The potential overlap scores for each column, output as a 1D vector simulating a 2D matrix.
-    ///                                    The potential overlap scores are calculated without considering the synapse permanence values.
-    ///        
-    /// @return                            Void as the outputs (out_overlap and out_pot_overlap) are passed by reference.
-    //-----------------------------------------------------------------------------
-    void calculate_overlap_gpu_stream_opt_sparse(
-                                            const int columns_width, const int columns_height,
-                                            const std::vector<uint32_t> &colConBits,
-                                            const std::vector<Int2> &active_grid, // List of active elements as int2 coordinates
-                                            int num_active,
-                                            const std::pair<int, int> &inputGrid_shape,
-                                            const std::pair<int, int> &neib_shape,
-                                            const std::pair<int, int> &neib_step,
-                                            bool wrap_mode,
-                                            bool center_neigh,
-                                            std::vector<float> &out_overlap,
-                                            std::vector<float> &out_pot_overlap
-                                        );
+    // // Struct to represent a 2D integer coordinate. 
+    // // Used as an input type in the calculate_overlap_gpu_stream_opt_sparse function.
+    // // This struct is equivalent to cuda::int2 struct and is converted to cuda::int2 in the function calculate_overlap_gpu_stream_opt_sparse.
+    // struct Int2 {
+    //     int x;
+    //     int y;
+    // };
+    // //-----------------------------------------------------------------------------
+    // ///
+    // /// calculate_overlap_gpu_stream_opt_sparse   Calculate the overlap scores for a given sparse input.
+    // ///                                          This function calculates how well each cortical column connects to the active inputs.
+    // ///                                          It's optimized for streaming sparse data, where active inputs are a small subset of the total.
+    // ///                                          The function is designed for efficient processing with varying active input patterns but
+    // ///                                          fixed structural parameters (like cortical column dimensions and neighborhood size).
+    // ///                                          It uses a list of active elements instead of a dense input grid to minimize memory usage
+    // ///                                          and computation on inactive elements. 
+    // ///                                          It also uses a colConBits input parameter. This is a vector of uint32_t where each bit in the array represents the connection bit for a synapse in the neighbourhood.
+    // ///                                          This input parameter is designed to reduce the GPU memory footprint of the connected state of each synpase.
+    // ///
+    // /// @param[in] columns_width           The width of the cortical columns (number of columns in the output matrix).
+    // /// @param[in] columns_height          The height of the cortical columns (number of rows in the output matrix).
+    // /// @param[in] colConBits              A vector of uint32_t where each bit represents the connection state of a synapse in the neighbourhood.
+    // /// @param[in] active_grid             A vector of int2 structures representing the active elements' coordinates in the input grid.
+    // /// @param[in] num_active              The number of active elements in the active_grid.
+    // /// @param[in] inputGrid_shape         The shape of the full input grid (height, width) as a pair of ints, for context in processing the sparse active elements.
+    // /// @param[in] neib_shape              The shape of the neighbourhood around each cortical column.
+    // /// @param[in] neib_step               The step size of the sliding window over the input grid.
+    // /// @param[in] wrap_mode               Whether to wrap the neighbourhood around the edges (toroidal topology) or not.
+    // /// @param[in] center_neigh            Whether to center the neighbourhood around each active element or start from the corner.
+    // /// @param[out] out_overlap            The overlap scores for each column, output as a 1D vector simulating a 2D matrix.
+    // /// @param[out] out_pot_overlap        The potential overlap scores for each column, output as a 1D vector simulating a 2D matrix.
+    // ///                                    The potential overlap scores are calculated without considering the synapse permanence values.
+    // ///        
+    // /// @return                            Void as the outputs (out_overlap and out_pot_overlap) are passed by reference.
+    // //-----------------------------------------------------------------------------
+    // void calculate_overlap_gpu_stream_opt_sparse(
+    //                                         const int columns_width, const int columns_height,
+    //                                         const std::vector<uint32_t> &colConBits,
+    //                                         const std::vector<Int2> &active_grid, // List of active elements as int2 coordinates
+    //                                         int num_active,
+    //                                         const std::pair<int, int> &inputGrid_shape,
+    //                                         const std::pair<int, int> &neib_shape,
+    //                                         const std::pair<int, int> &neib_step,
+    //                                         bool wrap_mode,
+    //                                         bool center_neigh,
+    //                                         std::vector<float> &out_overlap,
+    //                                         std::vector<float> &out_pot_overlap
+    //                                     );
 
 } // namespace gpu_overlap
